40c95d58d4965fcb990c86d44a109a24f32eaed7
Moosa Hassan <moosa7021@gmail.com>
2020-03-14 18:36:33 +1100

    splitting file input

--- main.java
+++ main.java
@@ -4,20 +4,33 @@ import java.util.Scanner;
 class main{
 	public static void main(String[] args) throws IOException{
 		Scanner input = new Scanner(System.in);
-		polygon polygonTestObject = new polygon();
-		MyPolygons myPolygonsObject = new MyPolygons();
-		System.out.println(polygonTestObject.toString());
+		//MyPolygons myPolygonsObject = new MyPolygons();
+		//System.out.println(polygonTestObject.toString());
 		
-		// ------------------------------------------------------------------------------------------------------
+		// ------------------------------------------------------------------------------------------------------ FILE READING SECTION ---------------------------------------------------------------------
 		Scanner file = new Scanner(new File("inputfile.txt"));
+		String text = "";
 
-		String line1 = file.nextLine();
-		String line2 = file.nextLine();
-		String line3 = file.nextLine();
+		try{
+			while(file.hasNextLine()){
+				text = text + file.next() + " ";
+			}
+		}catch(Exception e){
+			System.out.println("Error with reading files");
+		}
 
-		System.out.println(line1);
-		System.out.println(line2);
-		System.out.println(line3);
+		String[] textSplit = text.split("P ");
+
+		for(int i = 1; i < textSplit.length;i++){
+			System.out.println(textSplit[i]);
+		}
+
+		/*
+		System.out.println(textSplit[0]);
+		System.out.println(textSplit[1]);
+		System.out.println(textSplit[2]);
+		System.out.println(textSplit[3]);
+		*/
 
 		file.close();
 		// ------------------------------------------------------------------------------------------------------
@@ -29,7 +42,7 @@ class main{
 		System.out.println();
 		System.out.println();
 		System.out.println();
-		System.out.println();
+		System.out.println("***************************************** FAKE OUTPUT ************************************");
 		System.out.println("Unsorted list");
 		System.out.println("[(4.00 , 0.00)(4.00 , 8.00)(7.00 , 8.00)(7.00 , 3.00)(9.00 , 0.00)(7.00 , 1.00)]: 24.50");
 		System.out.println("[(4.10 , 0.00)(4.00 , 8.20)(7.30 , 8.40)]: 13.54");
