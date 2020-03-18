62ac9130c60d81477bae83f6349ac426653f56f1
Moosa Hassan <moosa7021@gmail.com>
2020-03-18 19:26:49 +1100

    removed testing print statements + interface can now be run through polygon class

--- main.java
+++ main.java
@@ -15,28 +15,23 @@ class main{
 			while(file.hasNext()){
 				newText = file.next();
 				
-				if(newText.equals("P")){
-					System.out.println("the file read P");
-					
+				if(newText.equals("P")){					
 					int numOfPoints = file.nextInt();
 					polygon polygonObject = new polygon(numOfPoints);
 
-					System.out.println("numOfPoints: " + numOfPoints);
 					point[] pointArray = new point[numOfPoints];
 					
 					int i = 1, j = 0;
 					while(i <= numOfPoints){
 						double x = file.nextDouble();
 						double y = file.nextDouble();
-						System.out.print ("(" + x + ", " + y + ")  \t   ");
 						pointArray[j] = new point(x, y);
 						polygonObject.insertPoint(x, y);
-						System.out.println("distanceFromOrigin: " + pointArray[j].distanceFromOrigin());
 						j++;
 						i++;
 					}
 
-					System.out.println("polygonObject area: " + polygonObject.calculateArea());
+					System.out.println(polygonObject.toString());
 				}
 			}
 		}catch(Exception e){
