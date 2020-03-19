812b0a310ce19ca7b7ca4983ef85883c873f4b75
Moosa Hassan <moosa7021@gmail.com>
2020-03-20 01:40:03 +1100

    visual studio code auto-cleanup

--- point.java
+++ point.java
@@ -1,22 +1,20 @@
-import java.util.Scanner;
 import java.lang.Math ;
 
 public class point{
 	private double xCoord, yCoord, distance;
-	private String pointStatement;
-
-	public point(double x, double y){
+	
+	public point(double x, double y) {
 		xCoord = x;
 		yCoord = y;
 	}
 
-	public double distanceFromOrigin(){
+	public double distanceFromOrigin() {
 		distance = Math.sqrt(Math.pow(Math.abs(xCoord), 2) + Math.pow(Math.abs(yCoord), 2));
 		return distance;
 	}
 
-	public String toString(){
-		return pointStatement = "(" + xCoord + " , " + yCoord + ")";
+	public String toString() {
+		return "(" + xCoord + " , " + yCoord + ")";
 	}
 
 	public double getX(){
