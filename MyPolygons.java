1376504b72c1a901a562cbe18429e773719f06e6
Moosa Hassan <moosa7021@gmail.com>
2020-03-21 16:13:22 +1100

    old code

--- MyPolygons.java
+++ MyPolygons.java
@@ -1,86 +1,106 @@
-public class MyPolygons{
-	// declare private variables
-	private Node current;
-	private Node sentinel;
-	private int length;
-	private int nodePosition;
+public class MyPolygons
+{
+    // declare private variables
+    private Node current;
+    private Node sentinel;
+    private int length;
+    private int nodePosition;
 
-	// the one with an object of polygon being passed through
-	public MyPolygons() {
-		// instantiate private variables
-		current = null;
-		sentinel = null;
-		length = 0;
-		nodePosition = 0;
-	}
+    // the one with an object of polygon being passed through
+    public MyPolygons()
+    {
+        // instantiate private variables
+        current = null;
+        sentinel = null;
+        length = 0;
+        nodePosition = 0;
+    }
 
-	// return list of polygons as a string
-	public String toString() {
-		// declare and instantiate string
-		String printer = "";
+    // return list of polygons as a string
+    public String toString()
+    {
+        // declare and instantiate string
+        String printer = "";
 
-		// loop for each node
-		for (int i = 0; i < length; i++) {
-			// add string to polygon
-			printer = printer + current.getData().toString() + "\n";
-			// iterate to next node
-			next();
-		}
-		
-		// return string
-		return printer;
-	}
+        // loop for each node
+        for (int i = 0; i < length; i++)
+        {
+            // add string to polygon
+            printer = printer + current.getData().toString() + "\n";
+            // iterate to next node
+            next();
+        }
 
-	public void prepend(polygon polygonObject) {
-		append(polygonObject);
-		sentinel = sentinel.getPrevious();
-		reset();
-	}
+        // return string
+        return printer;
+    }
 
-	public void append(polygon polygonObject){
-		Node temp = new Node(polygonObject);
-		
-		if(length == 0){
-			sentinel = temp;
-			current = sentinel;
-			sentinel.setNext(sentinel);
-			sentinel.setPrevious(sentinel);
-			reset();
-		}else{
-			temp.setNext(sentinel);
-			temp.setPrevious(sentinel.getPrevious());
-			sentinel.getPrevious().setNext(temp);
-			sentinel.setPrevious(temp);
-			reset();
-		}
-		length++;
-	}
+    public void prepend(polygon polygonObject)
+    {
+        append(polygonObject);
+        sentinel = sentinel.getPrevious();
+        reset();
+    }
 
-	public MyPolygons insertSort(MyPolygons sL){								// note to self: double check prepend()
-		System.out.println("Sorted List:");
-		
-		MyPolygons sortedList = sL;
+    public void append(polygon polygonObject)
+    {
+        Node temp = new Node(polygonObject);
 
-		reset();				// set current as sentinel (first position) TOP LIST
-		
-		for(int i = 0; i < length; i++){
-			sortedList.reset();
-			if(sortedList.getLength() == 0){
-				sortedList.append(sentinel.getData());
-			}else if(current.getData().comesBefore(sortedList.current.getData())){
-				sortedList.prepend(current.getData());
-			}else{
-				for(int j = 0; j < sortedList.getLength(); j++){
-					if(current.getData().comesBefore(sortedList.getCurrent())){
-						sortedList.append(current.getData());
-						break;
-					}else{
-						sortedList.next();
-					}
-				}
-			}
-			next();
-		}
+        if (length == 0)
+        {
+            sentinel = temp;
+            sentinel.setNext(sentinel);
+            sentinel.setPrevious(sentinel);
+            reset();
+        }
+        else
+        {
+            temp.setNext(sentinel);
+            temp.setPrevious(sentinel.getPrevious());
+            sentinel.getPrevious().setNext(temp);
+            sentinel.setPrevious(temp);
+            reset();
+        }
+        length++;
+    }
+
+    public MyPolygons insertSort()
+    {                                // note to self: double check prepend()
+        System.out.println("Sorted List:");
+
+        MyPolygons sortedList = new MyPolygons();
+
+        reset();                // set current as sentinel (first position) TOP LIST
+
+        for (int i = 0; i < length; i++){
+            sortedList.reset();
+            if (sortedList.getLength() == 0)
+            {
+                sortedList.append(sentinel.getData());
+            }
+            else if (current.getData().comesBefore(sortedList.current.getData()))
+            {
+                sortedList.prepend(current.getData());
+            }
+            else
+            {
+                sortedList.next();
+
+                for (int j = 1; j < sortedList.getLength(); j++)
+                {
+                    if (current.getData().comesBefore(sortedList.getCurrent()))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        sortedList.next();
+                    }
+                }
+                sortedList.insert(current.getData());
+            }
+            next();
+        }
 		
 		/* GOD
 		for (int i = 0; i < length; i++){
@@ -134,85 +154,105 @@ public class MyPolygons{
 		}
 		*/
 
-		return sortedList;
-	}
+        return sortedList;
+    }
 
-	public int getLength(){
-		return length;
-	}
+    public int getLength()
+    {
+        return length;
+    }
 
-	// accessor method for position of node
-	public int getPosition(Node n){
-		// create temporary node
-		Node tempNode = sentinel;
-		
-		int i = 1;
-		while(i <= length){
-			if(tempNode == n){						// sentinel matches the input node
-				return i;
-			}else{
-				// iterate to next node
-				tempNode = tempNode.getNext();
-			}
-			i++;
-		}
-		return 1;
-	}
+    // accessor method for position of node
+    public int getPosition(Node n)
+    {
+        // create temporary node
+        Node tempNode = sentinel;
 
-	public void insert(polygon polygonObject){
-		// • insert before a specified (current) item
-		if(length == 0){
-			append(polygonObject);
-			current = sentinel;
-		}else{
-			Node tempNode = new Node(polygonObject);
-			tempNode.setPrevious(current.getPrevious());
-			tempNode.setNext(current);
-			current.getPrevious().setNext(tempNode);
-			current.setPrevious(tempNode);
-			length++;
-		}
-	}
+        int i = 1;
+        while (i <= length)
+        {
+            if (tempNode == n)
+            {                        // sentinel matches the input node
+                return i;
+            }
+            else
+            {
+                // iterate to next node
+                tempNode = tempNode.getNext();
+            }
+            i++;
+        }
+        return 1;
+    }
 
-	public polygon remove(){			// remove from head (AKA remove from sentinel)
-		// • take (then remove) an item from the head of the list
-		current = sentinel.getNext();
-		
-		if(length == 0){						// if there are no nodes in the linked list
-			// do nothing
-		}else if(length == 1){					// if there is 1 node in the linked list
-			sentinel = null;
-		}else{
-			Node temp = sentinel;
+    public void insert(polygon polygonObject)
+    {
+        // • insert before a specified (current) item
+        if (length == 0)
+        {
+            append(polygonObject);
+            current = sentinel;
+        }
+        else
+        {
+            Node tempNode = new Node(polygonObject);
+            tempNode.setPrevious(current.getPrevious());
+            tempNode.setNext(current);
+            current.getPrevious().setNext(tempNode);
+            current.setPrevious(tempNode);
+            length++;
+        }
+    }
 
-			sentinel.getNext().setPrevious(sentinel.getPrevious());
-			sentinel.getPrevious().setNext(sentinel.getNext());
-			
-			sentinel = current;
-	
-			length--;
-			return temp.getData();
-		}
-		return current.getData();
-	}
+    public polygon remove()
+    {            // remove from head (AKA remove from sentinel)
+        // • take (then remove) an item from the head of the list
+        current = sentinel.getNext();
 
-	// set next node of current as current current
-	public void next(){
-		current = current.getNext();
-	}
+        if (length == 0)
+        {                        // if there are no nodes in the linked list
+            // do nothing
+        }
+        else if (length == 1)
+        {                    // if there is 1 node in the linked list
+            sentinel = null;
+        }
+        else
+        {
+            Node temp = sentinel;
 
-	// reset position of circular doubly linked list
-	public void reset(){
-		current = sentinel;
-		nodePosition = 1;
-	}
+            sentinel.getNext().setPrevious(sentinel.getPrevious());
+            sentinel.getPrevious().setNext(sentinel.getNext());
 
-	// accessor methods
-	public polygon getCurrent(){
-		return current.getData();
-	}
+            sentinel = current;
 
-	public polygon getSentinel(){
-		return sentinel.getData();
-	}
+            length--;
+            return temp.getData();
+        }
+        return current.getData();
+    }
+
+    // set next node of current as current current
+    public void next()
+    {
+        current = current.getNext();
+    }
+
+    // reset position of circular doubly linked list
+    public void reset()
+    {
+        current = sentinel;
+        nodePosition = 1;
+    }
+
+    // accessor methods
+    public polygon getCurrent()
+    {
+        return current.getData();
+    }
+
+    public polygon getSentinel()
+    {
+        return sentinel.getData();
+    }
 }
